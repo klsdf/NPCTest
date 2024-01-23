8d4b5cc1a581ca27eb546f2e61ab3c34c7da9564
ycx <1796655849@qq.com>
2024-01-23 14:49:22 +0800

    增加行为树的结点需求

---
+++ BehaviorTreeTest/Assets/Scripts/NPCAction/展示表情.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum 表情
+{ 
+    生气,
+    害羞,
+    疑惑,
+}
+
+
+public class 展示表情 : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ BehaviorTreeTest/Assets/Scripts/NPCAction/转为某一个情绪.cs
@@ -0,0 +1,28 @@
+using BehaviorDesigner.Runtime.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum 情绪
+{ 
+    高兴,
+    普通,
+    生气
+}
+
+
+
+public class 转为某一个情绪 : Action
+{
+    public 情绪 qingxu;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
